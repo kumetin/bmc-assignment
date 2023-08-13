37060376f72cd2e37b38b4984b701b7f0109cc7b
kumetin <kumetix@gmail.com>
2023-08-14 02:41:17 +0300

    fixed MainHandler

--- src/main/java/com/bmc/assignment/main/MainHandler.java
+++ src/main/java/com/bmc/assignment/main/MainHandler.java
@@ -5,6 +5,7 @@ import com.bmc.assignment.main.utils.dataset.Dataset;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -14,6 +15,7 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.Objects;
 
+@Component
 public class MainHandler {
     
     private static final Logger logger = LoggerFactory.getLogger(MainHandler.class);
